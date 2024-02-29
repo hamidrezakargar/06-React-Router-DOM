acd9976a7714822593380607d61c008d518a0923
Pourya Zardosht <pooriyazardosht.812@gmail.com>
2024-02-29 16:38:07 +0330

    routing fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div>
       <h1>Home Page</h1>
-      <Link to="/posts">See latest posts?</Link>
+      <Link to="/app/posts">See latest posts?</Link>
     </div>
   );
 }
